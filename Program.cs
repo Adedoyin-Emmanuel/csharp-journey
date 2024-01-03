40258fd205c58ba19ed073b046e4c8c2714afc26
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-01-03 09:56:02 +0100

    Feat: Wrote my first C# code

--- Program.cs
+++ Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld{
+    class Program{
+        static void Main(string[] args){
+            Console.WriteLine("Hello world");
+        }
+    }
+}
