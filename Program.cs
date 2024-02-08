e912a61de3ffd64058d361260b6a8c21985269bb
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-02-08 02:55:36 +0100

    Feat: Used for loops

--- Program.cs
+++ Program.cs
@@ -8,6 +8,7 @@ namespace CSharpJourney{
         static void Main(string[] args){
             Console.WriteLine("Hello World!");
             Program2.Hello();
+            Program2.PrintArray();
         }
     }
 }
