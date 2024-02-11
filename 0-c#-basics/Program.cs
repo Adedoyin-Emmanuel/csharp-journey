e5d0d23e518540724c7c9654d7dc70a5beef986e
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-02-11 20:38:16 +0100

    Feat: Worked with constants

--- 0-c#-basics/Program.cs
+++ 0-c#-basics/Program.cs
@@ -19,8 +19,25 @@ namespace CSharpJourney{
 
         public static void PrintString(){
             string fullName = "Adedoyin Emmanuel";
+            const string surName = "Adedoyin";
 
+
+            Console.WriteLine(surName);
             Console.WriteLine(fullName);
+        }
+
+
+        public static void WorkingWithArray(){
+            int [] numbers =  [ 1, 2, 3, 4, 5, 6, 7, 8];
+
+            for(int i = 0; i < numbers.Length; i++){
+                Console.WriteLine(numbers[i]);
+            }
+
+        }
+
+        public static void workingWithList(){
+            
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace CSharpJourney{
             Program2.PrintArray();
             Program2.PrintString();
 
-            
+           // Program2.WorkingWithArray();
         }
     }
 }
