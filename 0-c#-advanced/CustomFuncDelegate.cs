6f35a801c1d8049ec8d665ce5658937fed793820
Adedoyin-Emmanuel <adedoyine535@gmail.com>
2024-05-15 14:10:12 +0100

    Feat: Implemented custom func and action delegates

--- 0-c#-advanced/CustomFuncDelegate.cs
+++ 0-c#-advanced/CustomFuncDelegate.cs
@@ -5,4 +5,14 @@ namespace CSharpJourney{
 
     public delegate TResult CustomFunc<in T, out TResult>(T arg);
 
+    public delegate TResult CustomFunc<in T1, in T2, out TResult>(T1 arg1, T2 arg2);
+
+    public delegate TResult CustomFunc<in T1, in T2, in T3, out TResult>(T1 arg1, T2 arg2, T3 arg3);
+
+    public delegate void CustomAction<in T>(T arg);
+
+    public delegate void CustomAction<in T1, in T2>(T1 arg1, T2 arg2);
+
+    public delegate void CustomAction<in T1, in T2, in T3>(T1 arg1, T2 arg2, T3 arg3);
+
 }
